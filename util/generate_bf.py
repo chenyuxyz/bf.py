5aae9d8b076d3853f72261483a2de4cf445b1d3d
Chen-Yu Yang <penguin7272000@gmail.com>
2015-02-01 23:49:59 +0800

    generate_int picks the min len from n-1 to n+1, 0 to 255 costs 8087 now!

--- util/generate_bf.py
+++ util/generate_bf.py
@@ -16,14 +16,22 @@ def prime_factorize(n):
     return [1] if n == 1 else _fac(n, [])
 
 def generate_int(n):
+    def _generate(n):
+        if n == 0:
+            return ""
+        bfcode = ""
+        primes = prime_factorize(n)
+        bfcode += "[>".join("+" * p for p in primes)
+        bfcode += "<-]" * (len(primes) - 1)
+        bfcode += ">" * (len(primes) - 1)
+        return bfcode
     if n == 0:
         return ""
-    bfcode = ""
-    primes = prime_factorize(n)
-    bfcode += "[>".join("+" * p for p in primes)
-    bfcode += "<-]" * (len(primes) - 1)
-    bfcode += ">" * (len(primes) - 1)
-    return bfcode
+    bfcodes = []
+    bfcodes.append(_generate(n-1)+"+")
+    bfcodes.append(_generate(n))
+    bfcodes.append(_generate(n+1)+"-")
+    return min(bfcodes, key=len)
 
 def generate_print_str(string):
     bfcode = ""
